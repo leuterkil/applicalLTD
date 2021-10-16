3e1d7824d18009894318ec8566cfaa804fa6f547
Lefteris Charissopoulos <djfreedomofficial@gmail.com>
2021-10-16 11:06:19 +0300

    Finish MakeNewOrder In Controller

--- Server/controllers/order.js
+++ Server/controllers/order.js
@@ -1,6 +1,20 @@
 const Order = require('../models/order');
 
-module.exports.newOrder = async (req,res,next) =>
-{
-    const {orderDate,customer,frame}
-}
+module.exports.newOrder = async (req, res, next) => {
+  try {
+    const { orderDate, address, notes } = req.body;
+    const { content } = req.body.content;
+    const order = new Order(orderDate, address, notes);
+    order.content = content.map((detail) => ({
+      frameHeight: detail.frameHeight,
+      frameLength: detail.frameLength,
+      qty: detail.qty,
+      price: detail.price,
+      frameDesc: detail.frameDesc._id,
+    }));
+    await order.save;
+    res.json(order);
+  } catch (e) {
+    res.send(e);
+  }
+};
